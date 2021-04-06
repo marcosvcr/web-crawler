2594c049739f7b92997ea90265c8ace4cf2a8469
Marcos Cruz <marcoscr.bcc@gmail.com>
2021-04-05 23:27:41 -0300

    parallelized version second attempt

--- parallelized_crawler.py
+++ parallelized_crawler.py
@@ -1,3 +1,8 @@
+import unittest
+
+from nose.plugins.attrib import attr
+import nose
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import time
@@ -5,54 +10,144 @@ import pandas as pd
 import string
 import random
 import sys
-import asyncio
 
 
-async def fetch(url, session):
-    with session.get(url) as response:
-        time.sleep(5)
+name_agent = []
+version_agent = []
+locale = []
+platform = []
+timer = []
 
-        name_agent = await response.execute_script("return navigator.appName;")
-        WebDriverWait(session, 5).until(name_agent)
-        version_agent = await response.execute_script("return navigator.appVersion;")
-        WebDriverWait(session, 5).until(version_agent)
-        locale_agent = await response.execute_script("return Intl.DateTimeFormat().resolvedOptions().timeZone")
-        WebDriverWait(session, 5).until(locale_agent)
-        platform_agent = await response.execute_script("return navigator.platform")
-        WebDriverWait(session, 5).until(platform_agent)
-        timer_agent = await response.execute_script("return new Date()")
-        WebDriverWait(session, 5).until(timer_agent)
-        return {"name": name_agent, "version": version_agent, "locale": locale_agent, "platform": platform_agent, "timer":timer_agent}
 
-async def middlepoint(sem, url, session):
-    async with sem:
-        return await fetch(url, session)
 
-async def main():
-    driver = sys.argv[1]
-    maxpage = 50
-    num = 0
-    number_visited = 0
-    tasks = []
-    sem = asyncio.Semaphore(10)
-    with webdriver.Chrome(driver) as session:
-        session.implicitly_wait(30)
-        for i in range(0,maxpage):
+class Tests(unittest.TestCase):
+
+    @attr(batch=("1"))
+    def test_1(self):
+        driver = sys.argv[1]
+        webdriver.Chrome(driver)
+        maxpage = 10
+        number_visited = 0
+        num = 0
+        _response = True
+        while _response == True and number_visited < maxpage:
             letter = random.choice(list(string.ascii_lowercase))
-            number_visited = number_visited + 1
             num = num + 20
-            url = "http://www.portaldalinguaportuguesa.org/index.php?action=fonetica&region=spx&act=list&letter=" + letter + "&start=" + str(num)
-            print(number_visited, "Accessing", url)
-            tasks.append(
-                asyncio.create_task(
-                    middlepoint(sem,url,session)
-                )
-            )
+            number_visited= number_visited + 1
+            path = "http://www.portaldalinguaportuguesa.org/index.php?action=fonetica&region=spx&act=list&letter=" + letter + "&start=" + str(num) 
+            print(number_visited, "Accessing", path)
+            webdriver.get(path)
+            time.sleep(5)
+            name_agent.append(self.browser.execute_script("return navigator.appName;"))
+            version_agent.append(self.browser.execute_script("return navigator.appVersion;"))
+            locale.append(self.browser.execute_script("return Intl.DateTimeFormat().resolvedOptions().timeZone"))
+            platform.append(self.browser.execute_script("return navigator.platform"))
+            timer.append(self.browser.execute_script("return new Date()"))
+            self.assertTrue(_response == True)
+
+    @attr(batch=("2"))
+    def test_2(self):
+        driver = sys.argv[1]
+        webdriver.Chrome(driver)
+        maxpage = 10
+        number_visited = 0
+        num = 0
+        _response = True
+        while _response == True and number_visited < maxpage:
+            letter = random.choice(list(string.ascii_lowercase))
+            num = num + 20
+            number_visited= number_visited + 1
+            path = "http://www.portaldalinguaportuguesa.org/index.php?action=fonetica&region=spx&act=list&letter=" + letter + "&start=" + str(num) 
+            print(number_visited, "Accessing", path)
+            webdriver.get(path)
+            time.sleep(5)
+            name_agent.append(self.browser.execute_script("return navigator.appName;"))
+            version_agent.append(self.browser.execute_script("return navigator.appVersion;"))
+            locale.append(self.browser.execute_script("return Intl.DateTimeFormat().resolvedOptions().timeZone"))
+            platform.append(self.browser.execute_script("return navigator.platform"))
+            timer.append(self.browser.execute_script("return new Date()"))
+            self.assertTrue(_response == True)
+
+    @attr(batch=("3"))
+    def test_3(self):
+        driver = sys.argv[1]
+        webdriver.Chrome(driver)
+        maxpage = 10
+        number_visited = 0
+        num = 0
+        _response = True
+        while _response == True and number_visited < maxpage:
+            letter = random.choice(list(string.ascii_lowercase))
+            num = num + 20
+            number_visited= number_visited + 1
+            path = "http://www.portaldalinguaportuguesa.org/index.php?action=fonetica&region=spx&act=list&letter=" + letter + "&start=" + str(num) 
+            print(number_visited, "Accessing", path)
+            webdriver.get(path)
+            time.sleep(5)
+            name_agent.append(self.browser.execute_script("return navigator.appName;"))
+            version_agent.append(self.browser.execute_script("return navigator.appVersion;"))
+            locale.append(self.browser.execute_script("return Intl.DateTimeFormat().resolvedOptions().timeZone"))
+            platform.append(self.browser.execute_script("return navigator.platform"))
+            timer.append(self.browser.execute_script("return new Date()"))
+            self.assertTrue(_response == True)
+
+    @attr(batch=("4"))
+    def test_4(self):
+        driver = sys.argv[1]
+        webdriver.Chrome(driver)
+        
+        maxpage = 10
+        number_visited = 0
+        num = 0
+        _response = True
+        while _response == True and number_visited < maxpage:
+            letter = random.choice(list(string.ascii_lowercase))
+            num = num + 20
+            number_visited= number_visited + 1
+            path = "http://www.portaldalinguaportuguesa.org/index.php?action=fonetica&region=spx&act=list&letter=" + letter + "&start=" + str(num) 
+            print(number_visited, "Accessing", path)
+            webdriver.get(path)
+            time.sleep(5)
+            name_agent.append(self.browser.execute_script("return navigator.appName;"))
+            version_agent.append(self.browser.execute_script("return navigator.appVersion;"))
+            locale.append(self.browser.execute_script("return Intl.DateTimeFormat().resolvedOptions().timeZone"))
+            platform.append(self.browser.execute_script("return navigator.platform"))
+            timer.append(self.browser.execute_script("return new Date()"))
+            self.assertTrue(_response == True)
+    
+    @attr(batch=("5"))
+    def test_5(self):
+        driver = sys.argv[1]
+        webdriver.Chrome(driver)
+        maxpage = 10
+        number_visited = 0
+        num = 0
+        _response = True
+        while _response == True and number_visited < maxpage:
+            letter = random.choice(list(string.ascii_lowercase))
+            num = num + 20
+            number_visited= number_visited + 1
+            path = "http://www.portaldalinguaportuguesa.org/index.php?action=fonetica&region=spx&act=list&letter=" + letter + "&start=" + str(num) 
+            print(number_visited, "Accessing", path)
+            webdriver.get(path)
+            time.sleep(5)
+            name_agent.append(self.browser.execute_script("return navigator.appName;"))
+            version_agent.append(self.browser.execute_script("return navigator.appVersion;"))
+            locale.append(self.browser.execute_script("return Intl.DateTimeFormat().resolvedOptions().timeZone"))
+            platform.append(self.browser.execute_script("return navigator.platform"))
+            timer.append(self.browser.execute_script("return new Date()"))
+            self.assertTrue(_response == True)
+
+def get_cases():
+    return [Tests]
 
 
+def get_suite(cases):
+    suite = unittest.TestSuite()
+    for case in cases:
+        tests = unittest.defaultTestLoader.loadTestsFromTestCase(case)
+        suite.addTests(tests)
 
 
-results = asyncio.run(main())
-
-for result in results:
-    print(result)
+if __name__ == "__main__":
+    nose.main(suite=get_suite(get_cases()))
